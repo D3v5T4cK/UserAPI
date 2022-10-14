bde14f4f2b27940cca4eb41e96d8fea9fce5b3c2
SuhailSherief <suhailscreed@gmail.com>
2022-10-14 20:45:02 +0530

    added api routes

--- server/routes/router.js
+++ server/routes/router.js
@@ -11,5 +11,6 @@ route.post('/api/users', controller.create)
 route.get('/api/users', controller.find)
 route.put('/api/users/:id', controller.update)
 route.delete('/api/users/:id', controller.delete)
+route.post('/api/search', controller.search)
 
 module.exports = route
