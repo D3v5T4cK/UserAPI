2cace98b7bf550fb078c74aa0a8d5d8c9ac677b6
SuhailSherief <suhailscreed@gmail.com>
2022-10-14 20:43:45 +0530

    added generate user functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -44,3 +44,77 @@ if(window.location.pathname == "/"){
 
     })
 }
+
+async function getUser() {
+    alert("Random User added from API");
+    const re =  await fetch('https://randomuser.me/api');
+    const { results } =  await re.json();
+    console.log(results);
+    fetch('/api/users', {
+        method:'POST',
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify({
+            
+            "gender":results[0].gender,
+            "title":results[0].name.title,
+            "first" : results[0].name.first,
+            "last": results[0].name.last,
+            "city":  results[0].location.city,
+            "state": results[0].location.state,
+            "country": results[0].location.country,
+            "postcode": results[0].location.postcode,
+            "email": results[0].email,
+            "username": results[0].login.username,
+            "date": results[0].dob.date,
+            "age": results[0].dob.age,
+            "phonenumber": results[0].phone,
+            "password": results[0].login.password,
+            "picture":"daddy.png"
+        })
+    }).then(response => {
+        if (response.redirected) {
+            window.location.href = response.url;
+        }
+    })
+    .catch(function (err) {
+        console.info(err + " url: " + url);
+    });
+}
+
+function sendData(e)
+{
+    const searchResults = document.getElementById('searchResults')
+    const selectType = document.getElementById('selectType').value;
+    let match = e.value.match(/^[a-zA-Z0-9@._]*/);
+    let match2 = e.value.match(/\s*/);
+    if(match2[0] === e.value)
+    {
+        searchResults.innerHTML='';
+        return;
+    }
+    if(match[0] === e.value)
+    {
+        fetch('/api/search', {
+            method:'POST',
+            headers: {'Content-Type' : 'application/json'},
+            body: JSON.stringify({
+                type:selectType,
+                search:e.value
+            })
+            }).then(res => res.json()).then(data => {
+                let payload = data.result;
+                searchResults.innerHTML = '';
+                if(payload.length < 1)
+                {
+                    searchResults.innerHTML = '<p>Sorry no users found</p>';
+                    return;
+                }
+                payload.forEach(element => {
+                    searchResults.innerHTML += '<hr>';
+                    searchResults.innerHTML += `<a href="/user/${element._id}"><p>${element.name.first}</p></a>`
+                });
+        })
+        return;
+    }
+    searchResults.innerHTML = ''
+}
