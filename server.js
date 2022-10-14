eaa819f03e22d1c3917eb6a782f56c3724526438
SuhailSherief <suhailscreed@gmail.com>
2022-10-14 19:16:11 +0530

    miscellaneous changes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const path = require('path')
+const connectDB = require('./server/database/connection')
+const cors = require('cors')
 
 const app = express()
 
@@ -10,22 +12,14 @@ dotenv.config({path:'config.env'})
 const PORT = process.env.PORT||8080
 
 app.use(morgan("tiny"))
-app.use(bodyParser.urlencoded({extended: true}))
+connectDB()
+app.use(express.json())
+app.use(cors())
 app.set("view engine", "ejs")
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-app.get('/', (req, res)=>{
-    res.render('index')
-})
-
-app.get('/addUser', (req, res)=>{
-    res.render('addUser')
-})
-
-app.get('/updateUser', (req, res)=>{
-    res.render('updateUser')
-})
+app.use('/', require('./server/routes/router'))
 
 app.listen(3000, ()=>{console.log(`Server is running on http://localhost:${PORT}`)})
