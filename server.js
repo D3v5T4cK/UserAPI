541914e68d1e5f87c5672efe13efc76152d36af5
SuhailSherief <suhailscreed@gmail.com>
2022-10-14 08:43:12 +0530

    configured routes

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
+app.get('/', (req, res)=>{
+    res.render('index')
+})
 
+app.get('/addUser', (req, res)=>{
+    res.render('addUser')
+})
+
+app.get('/updateUser', (req, res)=>{
+    res.render('updateUser')
+})
 
 app.listen(3000, ()=>{console.log(`Server is running on http://localhost:${PORT}`)})
