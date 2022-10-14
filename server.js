c70b9727d1089f2bd919cfa039a0fcd2e1f3fd5f
SuhailSherief <suhailscreed@gmail.com>
2022-10-14 08:39:20 +0530

    imported static files

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const path = require('path')
+
 const app = express()
 
-app.get('/', (req, res)=>{
-    res.send("Root request working")
-})
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT||8080
 
-app.listen(3000, ()=>{console.log(`Server is running on http://localhost:${3000}`)})
+app.use(morgan("tiny"))
+app.use(bodyParser.urlencoded({extended: true}))
+app.set("view engine", "ejs")
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+
+
+
+app.listen(3000, ()=>{console.log(`Server is running on http://localhost:${PORT}`)})
