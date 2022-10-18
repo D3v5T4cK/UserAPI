cba8cc5dcd212ab22a253c6285c4a2fab621ef03
SuhailSherief <suhailscreed@gmail.com>
2022-10-18 19:45:53 +0530

    security patch update

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const morgan = require('morgan')
 const path = require('path')
 const connectDB = require('./server/database/connection')
 const cors = require('cors')
+let corsOptions = {
+    origin: 'https://d3v5t4ck.herokuapp.com/'
+  };
 const { use } = require('./server/routes/router')
 const dotenv = require('dotenv')
 const app = express()
@@ -13,7 +16,7 @@ const PORT = process.env.PORT||8080
 app.use(morgan("tiny"))
 connectDB()
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions));
 app.set("view engine", "ejs")
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
